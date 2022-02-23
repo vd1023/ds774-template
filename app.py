b886b6b674ff81d2dd9b2312c8333cf9b89eb8d4
ian newcombe <ian.newcombe@unh.edu>
2022-02-23 18:21:12 -0500

    init

---
+++ app.py
@@ -0,0 +1,97 @@
+from os import error
+import re
+from flask import Flask, render_template, request, url_for, redirect, session
+from functions.admin import contact_form,login_user, get_records, get_single_record, edit_record, delete_record
+
+app = Flask(__name__)
+
+app.secret_key = "IAN"
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/courses")
+def courses():
+    return render_template('courses.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    message = ''
+    if request.method == 'POST':
+        fname = request.form['fname']
+        lname = request.form['lname']
+        eaddress = request.form['eaddress']
+        message = request.form['message']
+        result = contact_form(fname, lname, eaddress, message)
+
+        if result:
+            return render_template('contact.html', message='Thank you for your submission')
+        else:
+            return render_template('contact.html', message='Error with submission')
+    else:
+        return render_template('contact.html', message=message)
+
+@app.route("/admin", methods=['GET', 'POST'])
+def admin():
+    error = ''
+    records = ''
+    print(request)
+
+    # If method was POST, a form was submitted
+    if request.method == 'POST':
+
+        # If the form was Login, perform log in steps
+        if request.form.get('admin') == 'Login':
+            username = request.form['username']
+            password = request.form['password']
+
+            # pass username and password from form to our login logic
+            result = login_user(username, password)
+
+            # If login was successful, create a session for the user, and load data, show data onpage
+            if result:
+                session['user_id'] = result
+                records = get_records()
+                # print(records)
+            
+            # login was not sucessful, show error message
+            else:
+                error = 'Invalid Username or Password'
+        
+        # if form was logout button, end user session
+        elif request.form.get('admin')  == 'Logout':
+            session.pop('user_id')
+
+        
+    # if user is logged in previously, show data. If no session, data is not retireved
+    if 'user_id' in session:
+        records = get_records()
+
+    # return the admin page, showing any message or data that we may have
+    return render_template('admin.html', error = error, records = records)
+
+@app.route("/edit", methods=['GET', 'POST'])
+def edit():
+    msg_id = request.args['id']
+    if request.method == 'POST':
+        if request.form.get('edit') == 'save':
+            fname = request.form['fname']
+            lname = request.form['lname']
+            eaddress = request.form['eaddress']
+            message = request.form['message']
+            print(fname, lname, eaddress, message)
+            edit_record(msg_id, fname, lname, eaddress, message)
+            return redirect('/admin')
+
+        elif request.form.get('edit') == 'cancel':
+            return redirect('/admin')
+        
+        elif request.form.get('admin') == 'Delete':
+            delete_record(msg_id)
+            return redirect('/admin')
+
+
+    entry = get_single_record(msg_id)
+
+    return render_template('edit.html', record = entry)
---
+++ functions/admin.py
@@ -0,0 +1,168 @@
+import os
+import psycopg2
+
+def connect_to_db():
+    connection = None
+     # Connect to database
+    try:
+        # get database URL dynamically from Heroku
+        DATABASE_URL = os.environ['DATABASE_URL']
+        
+        # create connection with database
+        connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    except:
+        print('error connecting to database')
+    
+    return connection
+
+
+def contact_form(fname, lname, eaddress, message):
+    
+    conn = connect_to_db()
+
+    sql = f"INSERT INTO contacts(fname,lname,eaddress,message) VALUES('{fname}', '{lname}', '{eaddress}', '{message}');"
+    try:
+
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql)
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+        return True
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return False
+    finally:
+        if conn is not None:
+            conn.close()
+
+def login_user(user, password):
+    
+    conn = connect_to_db()
+
+    sql = f"SELECT * FROM users WHERE username ='{user}' and password = '{password}';"
+
+    try:
+
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the SELECT statement
+        cur.execute(sql)
+        # get the selected users back
+        users = cur.fetchall()
+        # close communication with the database
+        cur.close()
+
+        # check if user credentials were correct
+        if len(users) == 1:
+            return users[0][0]
+        else:
+            return False
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def get_records():
+    conn = connect_to_db()
+
+    sql = f"SELECT * FROM contacts ORDER BY message_id ASC;"
+
+    try:
+
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the SELECT statement
+        cur.execute(sql)
+        # get the data back
+        records = cur.fetchall()
+        # close communication with the database
+        cur.close()
+        
+        # return data
+        return records
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def get_single_record(message_id):
+    conn = connect_to_db()
+
+    sql = f"SELECT * FROM contacts where message_id = '{message_id}';"
+
+    try:
+
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the SELECT statement
+        cur.execute(sql)
+        # get the selected record back
+        records = cur.fetchone()
+        # close communication with the database
+        cur.close()
+
+        # return retieved record
+        return records
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def edit_record(message_id, fname, lname, eaddress, message):
+    conn = connect_to_db()
+
+    sql = f"update contacts set fname='{fname}',lname='{lname}',eaddress='{eaddress}',message='{message}' where message_id='{message_id}';"
+
+    try:
+
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the SELECT statement
+        cur.execute(sql)
+        # commit changes to database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+        
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def delete_record(message_id):
+    
+    conn = connect_to_db()
+
+    sql = f"DELETE FROM contacts WHERE message_id = {message_id};"
+
+    try:
+
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the SELECT statement
+        cur.execute(sql)
+        # commit changes to database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    
